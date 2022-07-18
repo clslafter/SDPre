9d162036ab606e1ee9920342f51662d0d1ad6bb8
clslafter <clslafter@att.net>
2022-07-18 15:19:10 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Celicia";
+    int age = 40;
+    String state = "Louisiana";
+
+      System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
