60ff0446362d090aa6b34cc4a09f55a829ad5ab7
clslafter <clslafter@att.net>
2022-07-18 15:34:40 -0500

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int birthYear = 1981;
+    String birthMonth = "November";
+    boolean isTodayWeekend = false;
+
+      System.out.println(birthYear);
+      System.out.println(birthMonth);
+      System.out.println(isTodayWeekend);
+  }
+}
