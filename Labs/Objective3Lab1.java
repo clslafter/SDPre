078feb07cf8b1c318734afb80446c5c2f5ef3304
clslafter <clslafter@att.net>
2022-07-19 14:59:51 -0500

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 15;
+    int num2 = 6;
+    int num3 = 2;
+    double average = (num1 + num2 + num3) / 3;
+
+      System.out.println(average);
+  }
+}
