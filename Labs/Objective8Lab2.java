16520d4257849053ea2682c1124b9fcae13e8cf2
clslafter <clslafter@att.net>
2022-07-26 12:15:12 -0500

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum =0;
+
+    for(int control=1; control<=20; control+=1) {
+      sum = sum + control;
+    }
+
+    System.out.println(sum);
+
+  }
+}
